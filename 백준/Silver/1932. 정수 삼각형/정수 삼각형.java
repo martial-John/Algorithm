f0832e45c4165473bc4a1966aac67dac0b4e8b28
컨셉입니다 <testserver1444@gmail.com>
2025-10-22 19:21:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 232 ms, Memory: 25668 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,35 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[N+1][N+1];
-		int[][] DP = new int[N+1][N+1];
-		for(int i = 1 ; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < i; j++) {
-				arr[i-1][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		DP[0][0] = arr[0][0];
-		for(int i = 1; i < N; i++) {
-			for(int j = 0; j <= i; j++) {
-				switch(j) {
-				case 0 : DP[i][j] = DP[i-1][j] + arr[i][j]; break;
-				default : DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + arr[i][j]; break;
-				}
-			}
-		}
-		
-		int ans = 0;
-		for(int c : DP[N-1]) ans = Math.max(ans, c);
-			
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < i+1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < i+1; j++) {
+                arr[i][j] += + Math.max(arr[i+1][j], arr[i+1][j+1]);
+            }
+        }
+        System.out.print(arr[0][0]);
+    }
 }
